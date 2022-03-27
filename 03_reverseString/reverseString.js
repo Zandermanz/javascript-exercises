b1a4ab8b44e941d554232605e700cc9e3b6f7aa3
Andrew Phillips <aphillips15@gmail.com>
2022-03-26 23:12:50 -0700

    Solved reverse String problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  //take last letter of string and add it to reversedString one at a time, until .length is reached
+  //Not zero indexed, .length measures full number of characters
+  stringLength = string.length;
+  //set what will be return to a blank string.
+  let reversedString = "";
+  //loop starts at the last number, taking last character and putting it in reversed string
+  for(let i = stringLength; i > 0 ; i--){
+    reversedString = reversedString + string.slice(i-1, i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
