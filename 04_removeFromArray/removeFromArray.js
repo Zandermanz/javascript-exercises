44dcfe2c986f280804f714f675b251f79440382d
Andrew Phillips <aphillips15@gmail.com>
2022-03-30 22:46:18 -0700

    Adding looping mechanism to take multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function(array, remove) {
-let deletedArray = array.splice(remove-1, 1);
-return array;
+const removeFromArray = function(array, ...remove) {
+    //take remove as a rest parameter to put in all the things to remove
+    //then loop through the rest parameter to take out each "argument"
+    for (let i = 0; i <= remove.length-1; i++){
+        let deletedArray = array.splice(remove-1, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
