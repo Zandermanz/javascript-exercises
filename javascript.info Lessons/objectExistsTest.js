bcadc0b7216c1129b3c8047b8b6dab0433104679
Andrew Phillips <aphillips15@gmail.com>
2022-04-19 21:41:54 -0700

    basic setup

---
+++ javascript.info Lessons/objectExistsTest.js
@@ -0,0 +1,12 @@
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+
+function isEmpty(schedule){
+    //write function that checks whether object is empty
+
+}
