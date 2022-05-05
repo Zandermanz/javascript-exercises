060e3cb7f5b2f8f3d64c6086d75e3070ed80c9ac
Andrew Phillips <aphillips15@gmail.com>
2022-05-04 23:05:47 -0700

    solved calculator functiions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const { run } = require("jest-cli");
+
+const add = function(a, b) {
+return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let runningTotal = 0;
+  array.forEach(element => runningTotal = runningTotal + element);
+  return runningTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  //starts at 1 rather than zero, to avoid multiplication by zero
+  let runningTotal = 1;
+  array.forEach(element => runningTotal = runningTotal * element);
+  return runningTotal;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	// factorial takes a number and multiplies all numbers leading up to it
+  // loop take in number, set it as the max, and then loop
+  if(number === 0 || number === 1) return 1; //factorial of 0 is also 1
+  let runningTotal = 1;
+  for (let index = 1; index <= number; index++) {
+    runningTotal = runningTotal * index;
+  }
+  return runningTotal;
 };
 
 // Do not edit below this line
