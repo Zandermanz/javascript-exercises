d83f51e1a1d8ae2d7280d987bdfdf015af074938
Andrew Phillips <aphillips15@gmail.com>
2022-05-07 15:33:57 -0700

    started palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+//take string, flip it, compare to original, If equal return true, otherwise false
 
+
+string = string.toUpperCase(); //converts strings to uppercase
+
+//starting from back or array, take off last letter and add it to reversedString
+let reversedString = ""; //defining reversed string before loop
+let stringLength = string.length; //defining length outside of loop
+for(i = stringLength; i > 0 ; i--){
+ let letter = string.slice(i);
+ reversedString = reversedString + letter;
+}
+console.log(reversedString)
+
+//punctuation to strip out
+// if (char == " " || char == "." ||char == "?" || char == "," ||char == "!"){
+//     //ignore it
+//     return;
+//}
+
+return (reversedString == string ? true : false);
 };
 
 // Do not edit below this line
