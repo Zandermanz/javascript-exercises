26ace20a1ae6dac686ecf65086348c8e513eea2e
Andrew Phillips <aphillips15@gmail.com>
2022-05-11 19:36:16 -0700

    solved fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+//take input, iterate over loop up to that number, popping next number into array, until number is hit.
+// 1, 1, 2, 3, 5, 8, 13...
+// fibonacci(4) should return 3
+    if (number <= 0) return "OOPS"
+    const fibonacci = [1, 1];
+    for(let i= 2; i <= number; i++){
+        fibonacci.push(fibonacci[i-2] + fibonacci[i-1])
+    }
+    return fibonacci[number-1];
 };
 
 // Do not edit below this line
